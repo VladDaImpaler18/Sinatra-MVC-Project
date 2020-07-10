f9686049d14be27f82f449493f630932203ec8cb
vlad <VladDaImpaler@gmail.com>
2020-07-10 18:44:07 -0400

    added project skeleton

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+gem 'sinatra'
+gem 'require_all'
+gem 'activerecord', '5.2'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'dotenv'
+gem 'bcrypt'
+
+group :development do
+   gem 'shotgun'
+   gem 'pry'
+   gem 'sqlite3', '~> 1.3.6'
+   gem 'tux'
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Cherryberry Tom and Jerry"
+  end
+
+
+  get "/" do
+  
+  end
+
+end
